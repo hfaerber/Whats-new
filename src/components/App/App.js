4bc2286bc2ce95217487aa27c372498e7a054c7c
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-18 19:02:21 -0700

    Import newscontainer and update state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import NewsContainer from './../NewsContainer/NewsContainer.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local: local
     }
   }
 
@@ -14,6 +15,9 @@ class App extends Component {
     return (
       <div className="app">
         YOUR CODE GOES HERE!
+        <NewsContainer
+          localData = {this.state.local}
+        />
       </div>
     );
   }
