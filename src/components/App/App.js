9a99912f249f6161bf54328baee3fb10ec259239
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-19 15:33:58 -0700

    Add imported data to state and begin building update method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from './../NewsContainer/NewsContainer.js'
+import Menu from './../Menu/Menu.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local: local
+      selectedNews: '',
+      local,
+      entertainment,
+      health,
+      science,
+      technology,
     }
+  }
+
+  selectNews = (newsType) => {
+    this.setState({ selectedNews: newsType });
+    console.log(this.state.local)
   }
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <Menu />
         <NewsContainer
           localData = {this.state.local}
         />
