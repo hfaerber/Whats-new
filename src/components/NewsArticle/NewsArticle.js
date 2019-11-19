bd7738e46922e9ca035382c7b810f73be37fd9fc
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-19 16:30:15 -0700

    Remove setup for logo in news article

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,13 +3,12 @@ import './NewsArticle.css';
 
 function NewsArticle(props) {
   return (
-    <div className="newsArticle">Article
+    <div className="newsArticle">
       <img src={props.articleImg} alt="stock"/>
       <h3>{props.articleHeadline}</h3>
       <p>{props.articleDescription}</p>
       <div className="linkBox">
         <a href={props.articleUrl}> Link To article</a>
-        <img className='logo' src="./../logo.svg" alt="arrow logo"/>
       </div>
     </div>
 )}
