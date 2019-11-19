e8e1545ff504f120c2ea2bcc1e1d52bd76fb3106
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-18 19:04:56 -0700

    Create newsarticle and preliminary styling for newsarticle

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+function NewsArticle(props) {
+  return (
+    <div className="newsArticle">Article
+      <img src={props.articleImg} alt="stock"/>
+      <h3>{props.articleHeadline}</h3>
+      <p>{props.articleDescription}</p>
+      <div className="linkBox">
+        <a href={props.articleUrl}> Link To article</a>
+        <img className='logo' src="./../logo.svg" alt="arrow logo"/>
+      </div>
+    </div>
+)}
 
 export default NewsArticle;
