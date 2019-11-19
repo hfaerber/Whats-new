4a186b47b7cd8cee71f2ce4abdadf8f5239eba0d
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-18 19:03:58 -0700

    Create newscontainer and preliminary styling for newscontainer

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from './../NewsArticle/NewsArticle.js'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+function NewsContainer(props) {
+  const newsArticles = props.localData.map(newsBit => {
+    return <NewsArticle
+      key = {newsBit.id}
+      articleImg = {newsBit.img}
+      articleHeadline = {newsBit.headline}
+      articleDescription = {newsBit.description}
+      articleUrl = {newsBit.url}
+      />
+  return newsArticles
+  })
+
+  return (
+    <main className='newsContainer'>
+    {newsArticles}
+    </main>
+  )
+}
 
 export default NewsContainer;
