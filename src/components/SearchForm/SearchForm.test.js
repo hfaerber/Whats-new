9afb17bc8332100ca24e1cb7fb794c32e10aa03a
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-21 16:41:16 -0700

    Add skeleton for next test

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -6,7 +6,28 @@ describe('SearchForm', () => {
   it('should match the snapshot with all data passed in correctly', () => {
     const wrapper = shallow(<SearchForm
       filterBySearch={jest.fn()}
-    />);
+      />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update state when handleChange is called', () => {
+    const mockFilterBySearch = jest.fn();
+    const wrapper = shallow(<SearchForm
+      filterBySearch={mockFilterBySearch}
+      />);
+    const mockEvent = { target: { value: 'pants' } };
+    const expected = 'pants';
+
+    wrapper.instance().handleChange(mockEvent);
+    expect(wrapper.state('searchQuery')).toEqual(expected);
+  })
+
+// Maybe??? not sure if needed based on today's lesson examples
+  // it('should invoke handleChange when clicked')
+
+  it('should call the filterBySearch prop with searchQuery state when clicked', () => {
+    
+  }
+  )
+
 })
