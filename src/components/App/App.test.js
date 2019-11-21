caa75bf57d3a3d5cea6e1b306936a479e09af06e
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-21 14:52:45 -0700

    Add tests to methods

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -7,4 +7,47 @@ describe('App', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update selectedNews state and filteredSelection state when setSelectedNews is called',
+    () => {
+    const wrapper = shallow(<App />)
+    const mockNewsType = 'testingNewsType'
+    const mockFilteredSelection = [ { id: 3, headline: 'testheadline',
+      img: 'testimg', description: 'testdesc', url: 'http://google.com' } ]
+
+    wrapper.setState( {filteredSelection: mockFilteredSelection} );
+
+    expect(wrapper.state('selectedNews')).toEqual('local');
+    expect(wrapper.state('filteredSelection')).toEqual(mockFilteredSelection);
+
+    wrapper.instance().setSelectedNews(mockNewsType);
+    expect(wrapper.state('selectedNews')).toEqual(mockNewsType);
+    expect(wrapper.state('filteredSelection')).toEqual([]);
+  })
+
+  it('should update filteredSelection state when filterBySearch is called',
+    () => {
+    const wrapper = shallow(<App />);
+    const mockEvent = { preventDefault: jest.fn() };
+    const mockSearchQuery = 'test';
+    const mockSelectedNews = 'pants'
+    const mockStartState = [
+      { id: 1, headline: 'headline', img: 'testimg1',
+        description: 'testdesc1', url: 'http://google.com' },
+      { id: 2, headline: 'test headline2', img: 'testimg2',
+        description: 'testdesc2', url: 'http://google.com' },
+      { id: 3, headline: 'headline3', img: 'testimg3',
+        description: 'testdesc3', url: 'http://google.com' } ]
+    const mockSearchedArticles = [
+      { id: 2, headline: 'test headline2', img: 'testimg2',
+        description: 'testdesc2', url: 'http://google.com' } ]
+    const expectedState = [
+      { id: 2, headline: 'test headline2', img: 'testimg2',
+        description: 'testdesc2', url: 'http://google.com' }]
+
+    wrapper.setState({selectedNews: mockSelectedNews, pants: mockStartState});
+    wrapper.instance().filterBySearch(mockEvent, mockSearchQuery);
+    expect(wrapper.state('filteredSelection')).toEqual(expectedState);
+  })
+
 })
